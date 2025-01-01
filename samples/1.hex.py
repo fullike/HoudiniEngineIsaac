85a4f2b19a79b9bfb1e3f9f9a416fad094561ae3
Jay <fullike@163.com>
2025-01-01 22:28:17 +0800

    initial commit

---
+++ samples/1.hex.py
@@ -0,0 +1,85 @@
+import numpy as np
+from isaacsim import SimulationApp
+
+simulation_app = SimulationApp({"headless": False})
+
+from omni.isaac.core import World
+from omni.isaac.core.objects import DynamicCuboid, VisualCuboid
+from pxr import Gf, Vt, UsdGeom
+
+my_world = World(stage_units_in_meters=1.0)
+
+import os, sys
+# import Python Libs and add Path for the Dll Files 
+sys.path.append("/opt/hfs20.5.445/houdini/python3.10libs")
+import hou
+import hapi
+from hei.he_geometry import HoudiniEngineGeometry
+from hei.he_manager import HoudiniEngineManager, SessionType
+
+def launch_hei():
+    he_manager = HoudiniEngineManager()
+    if not he_manager.startSession(1, he_manager.DEFAULT_NAMED_PIPE, he_manager.DEFAULT_TCP_PORT):
+        print("ERROR: Failed to create a Houdini Engine session.")
+        return
+    if not he_manager.initializeHAPI(True):
+        print("ERROR: Failed to initialize HAPI.")
+        return
+    otl_path = "{}/hda/hexagona_lite.hda".format(os.getcwd())
+    asset_name = he_manager.loadAsset(otl_path)
+    if asset_name is None:
+        print("Failed to load the default HDA.")
+        return
+    hexagona_node_id = 0
+    hexagona_part_id = 0
+    hexagona_cook = he_manager.createAndCookNode(asset_name, hexagona_node_id)
+#   he_manager.getAttributes(hexagona_node_id, hexagona_part_id)
+    plane_mesh = UsdGeom.Mesh.Define(simulation_app.context.get_stage(), "/proc_mesh")
+    points, indices = HoudiniEngineGeometry.readGeometryFromHoudini(he_manager.session, hexagona_node_id, he_manager.cook_options)
+    points_pxr = Vt.Vec3fArray(len(points) // 3)
+    indices_pxr = Vt.IntArray(len(indices))
+    for i in range(len(points) // 3):
+        points_pxr[i] = Gf.Vec3f(points[i*3+2], points[i*3], points[i*3+1])
+    plane_mesh.GetPointsAttr().Set(points_pxr)
+    plane_mesh.GetFaceVertexIndicesAttr().Set(indices)
+    plane_mesh.GetFaceVertexCountsAttr().Set([3] * (len(indices) // 3))
+
+launch_hei()
+cube_1 = my_world.scene.add(
+    VisualCuboid(
+        prim_path="/new_cube_1",
+        name="visual_cube",
+        position=np.array([0, 0, 0.5]),
+        size=0.3,
+        color=np.array([255, 255, 255]),
+    )
+)
+
+cube_2 = my_world.scene.add(
+    DynamicCuboid(
+        prim_path="/new_cube_2",
+        name="cube_1",
+        position=np.array([0, 0, 1.0]),
+        scale=np.array([0.6, 0.5, 0.2]),
+        size=1.0,
+        color=np.array([255, 0, 0]),
+    )
+)
+
+cube_3 = my_world.scene.add(
+    DynamicCuboid(
+        prim_path="/new_cube_3",
+        name="cube_2",
+        position=np.array([0, 0, 3.0]),
+        scale=np.array([0.1, 0.1, 0.1]),
+        size=1.0,
+        color=np.array([0, 0, 255]),
+        linear_velocity=np.array([0, 0, 0.4]),
+    )
+)
+
+my_world.scene.add_default_ground_plane()
+my_world.reset()
+while simulation_app.is_running():
+    simulation_app.update()
+simulation_app.close()
